d7d8272895107e9a69d316c560d9bd71d9be10fc
codingscode <heronnestg@hotmail.com>
2026-05-25 12:56:43 -0300

    +

--- A_teste001/src/Programa/Teste001.java
+++ A_teste001/src/Programa/Teste001.java
@@ -1,28 +1,23 @@
 package Programa;
 
+import java.util.HashMap;
+
 public class Teste001 {
 	
-	public static boolean presente(int[] l, int e) {
-		boolean b = false;
-		for (int cada : l) {
-			if (e == cada) {
-				b = b || true;
-			}
-		}
-		return b;
-	}
+	
+
 	
 	public static void main(String[] args) {
 		long startTime = System.nanoTime();
 		
-		int num1 = 3, num2 = 6, num3 = 7, num4 = 5;
+		HashMap<String, Integer> produto = new HashMap<>();
+		produto.put("preco", 10);
+		produto.put("quantidade", 3);
 		
-		int lista[] = {2, 3, 5, 8, 3, 10};
 		
-		System.out.println(presente(lista, num1));
-		System.out.println(presente(lista, num2));
-		System.out.println(presente(lista, num3));
-		System.out.println(presente(lista, num4));
+		System.out.println(produto);
+		
+
 		
 		System.out.println("--------------------------");
 		
@@ -42,15 +37,10 @@ public class Teste001 {
 }
 
 /*
-criar código para ver um se um numero está presente em um array int
-true
-false
-false
-true
---------------------------
---------------------------
-tempo execução em ms:
-0
+criar código 
+
+
+
 
 
 */
