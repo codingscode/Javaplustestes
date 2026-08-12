957d40b802c671cfd80b4ab538896a57e175defe
codingscode <heronnestg@hotmail.com>
2026-08-11 23:27:15 -0300

    +

--- Tutorial/feitosT/t140/Tut001.java
+++ Tutorial/feitosT/t140/Tut001.java
@@ -4,10 +4,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
-//Same functional interface
-interface Saldacao {
-	void digaOla();
-}
 
 public class Tut001 {
 
