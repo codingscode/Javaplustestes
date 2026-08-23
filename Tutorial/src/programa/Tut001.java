3b7e92508e45c17759e277d4f2b54d74a72f549a
codingscode <heronnestg@hotmail.com>
2026-08-23 00:31:57 -0300

    +

--- Tutorial/src/programa/Tut001.java
+++ Tutorial/src/programa/Tut001.java
@@ -1,23 +1,12 @@
 package programa;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class Tut001 {
 	
 	
 	public static void main(String[] args) {
 		long startTime = System.nanoTime();
 		
-		HashMap<String, Integer> produtos = new HashMap<>(Map.of(
-				"pendrive", 20, "ssd", 200, "mouse", 30, "teclado", 40, "cpu", 120));
 		
-		// Usando entrySet para percorrer o mapa
-		for (String chave : produtos.keySet()) {
-			Integer valor = produtos.get(chave);
-			
-		    
-		}
 		
 		
 		System.out.println("--------------------------");
@@ -37,13 +26,9 @@ public class Tut001 {
 }
 
 /*
-mouse: 30
-cpu: 120
-teclado: 40
-ssd: 200
-pendrive: 20
---------------------------
-tempo execução em ms: 0
+
+
+
 
 
 
